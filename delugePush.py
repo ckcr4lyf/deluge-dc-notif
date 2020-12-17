d3e972c1ac74c5f9543ef730944286bd821ab1c0
Raghu Saxena <poiasdpoiasd@live.com>
2020-12-17 08:04:47 +0100

    Change line index, print index to produce desired result on my machine

--- delugePush.py
+++ delugePush.py
@@ -64,8 +64,8 @@ def getState(torrentHash, torrentName, delayTime=0, delayCall=False):
     lines = delugeConsoleResult.split('\n')
 
     if (DELUGE_VERSION[0] == '2'):
-        if "Failed to connect to" in lines[1]:
-            print(lines[2]) # Prints the error from deluge back to the user
+        if "Failed to connect to" in lines[0]:
+            print(lines[0]) # Prints the error from deluge back to the user
             return
         tracker = lines[7][9:]
 
