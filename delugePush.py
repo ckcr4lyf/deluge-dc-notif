0caa21459c7161e61d0a9dec03c9b1a4c7253ba3
Emma Jennings <emma@0xem.ma>
2020-12-17 03:22:00 -0400

    Fix improper index of lines

--- delugePush.py
+++ delugePush.py
@@ -64,7 +64,7 @@ def getState(torrentHash, torrentName, delayTime=0, delayCall=False):
     lines = delugeConsoleResult.split('\n')
 
     if (DELUGE_VERSION[0] == '2'):
-        if "Failed to connect to" in lines[1]:
+        if "Failed to connect to" in lines[0]:
             print(lines[2]) # Prints the error from deluge back to the user
             return
         tracker = lines[7][9:]
